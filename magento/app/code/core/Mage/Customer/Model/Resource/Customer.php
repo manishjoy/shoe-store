ed8ff8583eee20a158e6a9857ebdfd82032db510
manishjoy <mjoy@excellencetechnologies.in>
2016-03-10 14:08:26 +0530

    abc

--- magento/app/code/core/Mage/Customer/Model/Resource/Customer.php
+++ magento/app/code/core/Mage/Customer/Model/Resource/Customer.php
@@ -199,7 +199,7 @@ class Mage_Customer_Model_Resource_Customer extends Mage_Eav_Model_Entity_Abstra
      * @return Mage_Customer_Model_Resource_Customer
      */
     public function loadByEmail(Mage_Customer_Model_Customer $customer, $email, $testOnly = false)
-    {  
+    {
         $adapter = $this->_getReadAdapter();
         $bind    = array('customer_email' => $email);
         $select  = $adapter->select()
@@ -225,16 +225,6 @@ class Mage_Customer_Model_Resource_Customer extends Mage_Eav_Model_Entity_Abstra
 
         return $this;
     }
-
-    // public function loadByPhone($phone)
-    // { // echo  $no;
-    //     $customer = Mage::getModel('customer/customer');
-    //     $collection = $customer->getCollection()->addAttributeToFilter('phone', $phone)->getData();
-    //     $arr = $collection;
-    //     foreach($collection as $cll) { $email = $cll['email']; }
-    //     return $email;
-    // }
-
 
     /**
      * Change customer password
